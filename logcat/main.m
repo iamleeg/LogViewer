fe23f9099303749648ddd52e7bbc76ff94982ddd
Graham Lee <iamleeg@securemacprogramming.com>
2012-06-14 11:35:34 +0100

    Add the helper tool.

---
+++ logcat/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  logcat
+//
+//  Created by Graham Lee on 14/06/2012.
+//  Copyright (c) 2012 Graham Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
