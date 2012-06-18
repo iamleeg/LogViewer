cdfebc21e823047ec1b992796c638d3c332d4ab4
Graham Lee <iamleeg@securemacprogramming.com>
2012-06-18 11:12:07 +0100

    Accept that this isn't at all a Foundation tool.

--- logcat/main.c
+++ logcat/main.c
@@ -6,79 +6,82 @@
 //  Copyright (c) 2012 Graham Lee. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#include <sys/types.h>
 #include <sys/event.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/uio.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <launch.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sysexits.h>
+#include <unistd.h>
 
 void exit_error(const char *message, const int error) __attribute__((noreturn));
 int listen_to_launchd_sockets();
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        int kernel_queue = listen_to_launchd_sockets();
-        
-        struct kevent listening_event;
-        int connected_socket = kevent(kernel_queue, NULL, 0, &listening_event, 1, NULL);
-        if (connected_socket == -1) {
-            exit_error("couldn't get the connected socket", errno);
-        }
-        
-        //accept the connection
-        struct sockaddr accepted_address = { 0 };
-        socklen_t address_length = 0;
-        
-        int accepted_socket = accept((int)listening_event.ident, &accepted_address, &address_length);
-        if (accepted_socket == -1) {
-            exit_error("couldn't accept the socket connection", errno);
-        }
-        //read and check the authorisation services rights
-        //read and check the one-byte command
-        char command = 0;
-        size_t bytes_read = recv(accepted_socket, &command, 1, 0);
-        if (bytes_read != 1) {
-            exit_error("couldn't read the command from the socket", errno);
-        }
-        //write the reply
-        int log_file = -1;
-        switch (command) {
-            case 's': {
-                log_file = open("/var/log/system.log", O_RDONLY);
-                break;
-            }
-            case 'i': {
-                log_file = open("/var/log/install.log", O_RDONLY);
-                break;
-            }
-            default: {
-                exit_error("unknown command", 0);
-                break;
-            }
-        }
-        if (log_file == -1) {
-            exit_error("couldn't open the log file", errno);
-        }
-        char *log_content = malloc(4096);
-        if (log_content == NULL) {
-            exit_error("couldn't allocate memory", errno);
-        }
-        size_t file_bytes_read = 0;
-        while ((file_bytes_read = read(log_file, log_content, 4096)) > 0) {
-            ssize_t socket_bytes_written = send(accepted_socket, log_content, file_bytes_read, 0);
-            if (socket_bytes_written < file_bytes_read) {
-                exit_error("couldn't write to socket", errno);
-            }
-        }
-        close(log_file);
-        free(log_content);
-        //close the connection
-        close(accepted_socket);
-        close(kernel_queue);
+    int kernel_queue = listen_to_launchd_sockets();
+    
+    struct kevent listening_event;
+    int connected_socket = kevent(kernel_queue, NULL, 0, &listening_event, 1, NULL);
+    if (connected_socket == -1) {
+        exit_error("couldn't get the connected socket", errno);
     }
+    
+    //accept the connection
+    struct sockaddr accepted_address = { 0 };
+    socklen_t address_length = 0;
+    
+    int accepted_socket = accept((int)listening_event.ident, &accepted_address, &address_length);
+    if (accepted_socket == -1) {
+        exit_error("couldn't accept the socket connection", errno);
+    }
+    //read and check the authorisation services rights
+    //read and check the one-byte command
+    char command = 0;
+    size_t bytes_read = recv(accepted_socket, &command, 1, 0);
+    if (bytes_read != 1) {
+        exit_error("couldn't read the command from the socket", errno);
+    }
+    //write the reply
+    int log_file = -1;
+    switch (command) {
+        case 's': {
+            log_file = open("/var/log/system.log", O_RDONLY);
+            break;
+        }
+        case 'i': {
+            log_file = open("/var/log/install.log", O_RDONLY);
+            break;
+        }
+        default: {
+            exit_error("unknown command", 0);
+            break;
+        }
+    }
+    if (log_file == -1) {
+        exit_error("couldn't open the log file", errno);
+    }
+    char *log_content = malloc(4096);
+    if (log_content == NULL) {
+        exit_error("couldn't allocate memory", errno);
+    }
+    size_t file_bytes_read = 0;
+    while ((file_bytes_read = read(log_file, log_content, 4096)) > 0) {
+        ssize_t socket_bytes_written = send(accepted_socket, log_content, file_bytes_read, 0);
+        if (socket_bytes_written < file_bytes_read) {
+            exit_error("couldn't write to socket", errno);
+        }
+    }
+    close(log_file);
+    free(log_content);
+    //close the connection
+    close(accepted_socket);
+    close(kernel_queue);
     return 0;
 }
 
